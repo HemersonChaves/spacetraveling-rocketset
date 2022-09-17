cd14a2b945aa7c0c03fe519b2479df1ea4a7a971
Hemerson Chaves <hemersonchaves20@hotmail.com>
2022-09-16 21:49:47 -0300

    refactor(home): adiciona base html dos posts

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,7 @@
 import { GetStaticProps } from 'next';
-
+import Head from 'next/head';
+import Link from 'next/link';
+import Image from 'next/image';
 import { getPrismicClient } from '../services/prismic';
 
 import commonStyles from '../styles/common.module.scss';
@@ -24,9 +26,38 @@ interface HomeProps {
   postsPagination: PostPagination;
 }
 
-// export default function Home() {
-//   // TODO
-// }
+export default function Home(): JSX.Element {
+  return (
+    <>
+      <Head>
+        <title>Space Traveling | Rocketseat</title>
+      </Head>
+      <main className={styles.contentContainer}>
+        <section className={styles.hero}>
+          <h1>
+            <img src="/images/logo.svg" alt="space travelin" /> spacetraveling
+            <span>.</span>
+          </h1>
+        </section>
+        <article className={styles.posts}>
+          <div>
+            <Link href="/">
+              <a>
+                <h1>titulo post</h1>
+                <div> post opijf opjf pojm </div>
+                <div>
+                  <time>12/12/12</time>
+                  <span>person</span>
+                </div>
+
+              </a>
+            </Link>
+          </div>
+        </article>
+      </main>
+    </>
+  );
+}
 
 // export const getStaticProps = async () => {
 //   // const prismic = getPrismicClient({});
