6750a2d39f52a48100ad260c11157fbb1756e790
Hemerson Chaves <hemersonchaves20@hotmail.com>
2022-09-25 16:18:07 -0300

    teste(slug): should be able to render post document info

--- src/pages/post/[slug].tsx
+++ src/pages/post/[slug].tsx
@@ -2,6 +2,8 @@ import { GetStaticPaths, GetStaticProps } from 'next';
 import Head from 'next/head';
 import { useRouter } from 'next/router';
 import { RichText } from 'prismic-dom';
+import { format } from 'date-fns';
+import ptBR from 'date-fns/locale/pt-BR';
 import { getPrismicClient } from '../../services/prismic';
 import commonStyles from '../../styles/common.module.scss';
 import styles from './post.module.scss';
@@ -28,6 +30,23 @@ interface PostProps {
 }
 
 export default function Post({ post }: PostProps): JSX.Element {
+  const date = format(new Date(post.first_publication_date), 'd MMM yyyy', {
+    locale: ptBR,
+  });
+  const tempoEstimado = Math.ceil(
+    post.data.content.reduce(
+      (acc, contentItem) =>
+        acc +
+        contentItem.heading.toString().split(' ').length +
+        contentItem.body.reduce(
+          (acc2, bodyItem) => acc2 + bodyItem.text.toString().split(' ').length,
+          0
+        ),
+      0
+    ) / 200
+  );
+
+  console.log(`total ${tempoEstimado}`);
   const router = useRouter();
   if (router.isFallback) {
     return <div>Carregando...</div>;
@@ -40,7 +59,9 @@ export default function Post({ post }: PostProps): JSX.Element {
       <main className={styles.container}>
         <article className={styles.post}>
           <h1>{post.data.title}</h1>
-          <time>{post.first_publication_date}</time>
+          <span>{post.data.author} </span>
+          <span>{`${tempoEstimado} min`} </span>
+          <time>{date}</time>
           <div className={styles.postContent}>
             {post.data.content.map(content => (
               <div key={content.heading}>
